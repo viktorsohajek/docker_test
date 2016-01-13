53485f1580afc1c887cf7dd62af50d8abf649a57
viktorsohajek <sohajek.viktor@gmail.com>
2016-01-13 19:14:49 +0100

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,1 +1,8 @@
-print "Hello world!"
+import csv
+
+with open('test.csv', 'w') as fp:
+    a = csv.writer(fp, delimiter=',')
+    data = [['Me', 'You'],
+            ['293', '219'],
+            ['54', '13']]
+    a.writerows(data)
