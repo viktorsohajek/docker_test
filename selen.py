aa7b735867b8481942cca64189eec6fbf8b9d98d
viktorsohajek <sohajek.viktor@gmail.com>
2016-04-04 12:54:09 +0200

    update - nova architektura, config.json

--- selen.py
+++ selen.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from keboola import docker # pro komunikaci s parametrama a input/output mapping
 import os 
 import datetime
 from datetime import date, timedelta
@@ -12,36 +13,76 @@ from pyvirtualdisplay import Display
 
 print "TEST OK"
 
-datum= str(time.strftime("%Y-%m-%d"))
-
 display = Display(visible=0, size=(1024, 768))
 display.start()
 
-### LIST SHOPU ###
+
+
+# initialize KBC configuration 
+cfg = docker.Config('src/data/')
+# loads application parameters - user defined
+parameters = cfg.get_parameters()
+
+
+### PARAMETERS ####
+
+#date
+scrape_date = str(time.strftime("%Y-%m-%d"))
 yesterday = date.today() - timedelta(1)
 date = yesterday.strftime('%Y-%m-%d')
 
+#mode
+mode = parameters.get('Mode')
+#mode = 'summary'
+#mode = 'by_category'
+
+### DEFINITION OF PARAMETERS ###
+#user input - cesta k souboru, kam se maji statistiky ukladat
+save_path="/src/data/out/"
+#set current date as "today - delta"
+delta=2
+current_date = str((datetime.datetime.now()-timedelta(delta)).date())
+
+#set stats dates
+#date_from = '2016-01-01'
+#date_to = '2016-02-16'
+
+
+
+# date format checker - vyhodi chybu pokud stats_date nebude Y-m-d
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
+
+#initialize stats_dates vector
+stats_dates={}
+
+#date preset from input parameters. Bud date_preset='Yesteday' nebo vsechny stats_dates ve stanovenem intervalu
+#! parametr 'date_preset' ma prednost.
+if parameters.get('Date_preset')=='Yesterday':
+    yesterday = date.today() - timedelta(1)
+    stats_dates[0] = yesterday.strftime('%Y-%m-%d')
+else:
+    validate(parameters.get('Date_from'))
+    validate(parameters.get('Date_to'))
+    d1=datetime.datetime.strptime(parameters.get('Date_from'),'%Y-%m-%d')
+    d2=datetime.datetime.strptime(parameters.get('Date_to'),'%Y-%m-%d')
+    delta = d2 - d1
+    for i in range(delta.days+1):
+        stats_dates[i]=(d1+timedelta(i)).strftime('%Y-%m-%d')
+
+
+
+### /DEFINITION OF PARAMETERS ###
+
+
+
+### LIST SHOPU ###
 dict={}
-dict['Heureka.cz'] = {}
 dict['Zbozi.cz'] = {}
-dict['Heureka.cz']['Login_1'] = {'Login': 'valiska@sportmall.cz',
-                     'Password': 'heurech15',
-                     'Url_login': 'https://login.heureka.cz/login',
-                     'Shop': ['Sporty.cz','Snowboards.cz','Kolonial.cz','Prodeti.cz'],
-                     'Url_stats': ['http://sluzby.heureka.cz/obchody/statistiky/?shop=5709&from='+date+'&to='+date+'&cat=-4','http://sluzby.heureka.cz/obchody/statistiky/?shop=1786&from='+date+'&to='+date+'&cat=-4','http://sluzby.heureka.cz/obchody/statistiky/?shop=53090&from='+date+'&to='+date+'&cat=-4','http://sluzby.heureka.cz/obchody/statistiky/?shop=45555&from='+date+'&to='+date+'&cat=-4'] }
-
-dict['Heureka.cz']['Login_2'] = {'Login': 'heureka@bigbrands.cz',
-                     'Password': 'ecommerceheureka',
-                     'Url_login': 'https://login.heureka.cz/login',
-                     'Shop': ['BigBrands.cz'],
-                     'Url_stats': ['http://sluzby.heureka.cz/obchody/statistiky/?from='+date+'&to='+date+'&shop=42893&cat=-4'] }
-
-dict['Heureka.cz']['Login_3'] = {'Login': 'info@limal.cz',
-                     'Password': 'heureka123456',
-                     'Url_login': 'https://login.heureka.cz/login',
-                     'Shop': ['Rozbaleno.cz'],
-                     'Url_stats': ['http://sluzby.heureka.cz/obchody/statistiky/?from='+date+'&to='+date+'&shop=6590&cat=-4'] }
-
 dict['Zbozi.cz']['Login_1'] = {'Login': 'it@snowboards.cz',
                      'Password': 'wsappcsk25',
                      'Url_login': '',
@@ -71,21 +112,6 @@ dict['Zbozi.cz']['Login_3'] = {'Login': 'ppc.bigbrands.cz@gmail.com',
 ### /LIST SHOPU ###
 
 
-### DEFINICE ###
-#user input - cesta k souboru, kam se maji statistiky ukladat
-save_path="/src/data/"
-#set current date as "today - delta"
-delta=2
-current_date = '{d.day}.{d.month}.{d.year}'.format(d=datetime.datetime.now()-timedelta(delta))
-#set stats dates
-date_from = current_date
-date_to = current_date
-
-#account info
-username = "it@snowboards.cz"
-password = "wsappcsk25"
-
-### /DEFINICE ###
 
 
 #zmeni working directory na slozku, kam se ukladaji statistiky
@@ -126,19 +152,34 @@ def lookup(driver, query1,query2):
 ### BROWSER START FCN ###
 
 
-def scrape(driver,web_id,shop_name):
+def scrape(driver,web_id,shop_name,date_from,date_to):
+    #check if the dates from and to differs for the by_category extraction
+    if mode == 'by_category' and (date_from!=date_to):
+        driver.close()
+        exit('Error: Extraction query for category view was called while the date_from and date_to differs.')
     if os.path.isfile("out_zbozi_stats_"+shop_name+".csv"):
-        os.rename("out_zbozi_stats_"+shop_name+".csv","zbozi_stats_"+shop_name+"_"+datetime.datetime.now().isoformat()+".csv")
+    #    os.rename("out_zbozi_stats_"+shop_name+".csv","zbozi_stats_"+shop_name+"_"+datetime.datetime.now().isoformat()+".csv")
+        os.rename("out_zbozi_stats_"+shop_name+".csv","prior"+"_"+"zbozi_stats_"+shop_name+".csv")    
+
+    # for urls - needs d.m.Y format
+    date_from_url = '{d.day}.{d.month}.{d.year}'.format(d=datetime.datetime.strptime(date_from, '%Y-%m-%d'))
+    date_to_url = '{d.day}.{d.month}.{d.year}'.format(d=datetime.datetime.strptime(date_to, '%Y-%m-%d'))
 
     #definice linku pro stahnuti
-    link_web_stats = 'https://admin.zbozi.cz/premiseStatistics?premiseId='+web_id+'&dateFrom='+date_from+'&dateTo='+date_to
+    link_web_stats = 'https://admin.zbozi.cz/premiseStatistics?premiseId='+web_id+'&dateFrom='+date_from_url+'&dateTo='+date_to_url
 
     #zavola link na stranku statistik
     driver.get(link_web_stats)
     time.sleep(5)
 
-    #zaskrtne radio pole "po kategoriich"
-    driver.find_element_by_xpath("//input[@id='sg']").click()
+    if mode == 'summary':
+        #zaskrtne radio pole "summary"
+        driver.find_element_by_xpath("//input[@id='sd']").click()
+
+    if mode == 'by_category':
+        #zaskrtne radio pole "po kategoriich"
+        driver.find_element_by_xpath("//input[@id='sg']").click()
+
     #stickne button na vygenerovani reportu
     driver.find_element_by_xpath("//input[@id='createNewReport']").click()
 
@@ -165,39 +206,147 @@ def scrape(driver,web_id,shop_name):
             os.rename(filename,"zbozi_stats_"+shop_name+".csv")
 
 
+#predelavac csvcek do slusny podoby
+def csv_handeler(mode,stats_date):
+    for filename in os.listdir(save_path):
+        if filename.endswith(".csv") and not (filename.startswith("out_") or filename.startswith("prior_")):
+            with open(save_path+filename,'r') as csvinput:
+                with open(save_path+'out_'+filename, 'w') as csvoutput:
+                    writer = csv.writer(csvoutput, lineterminator='\n',delimiter=",")
+                    reader = csv.reader(csvinput,delimiter=";")
+
+                    all = []
+
+                    if mode == 'summary':
+                        next(reader,None)
+                        writer.writerow(['date','clicks','impressions','ctr','cpc_avg','cpc_all','position_avg','conversion_count','conversion_value','converion_rate','conversion_avg_price','scrape_date'])
+
+                        for row in reader:
+                            row.append(scrape_date)
+                            all.append(row)
+
+                    if mode == 'by_category':
+                        next(reader,None)
+                        writer.writerow(['category_id','category_name','impressions','clicks','cpc_all','conversion_count','date','scrape_date'])
+                        for row in reader:
+                            row.append(stats_date)
+                            row.append(scrape_date)
+                            all.append(row)
+                    writer.writerows(all)
+            #appends the previous table to the new one        
+            if (os.path.isfile('prior_'+filename)):
+                with open(save_path+'prior_'+filename, 'r') as csvinput:
+                    with open(save_path+'out_'+filename, 'a') as csvoutput:
+                        writer = csv.writer(csvoutput, lineterminator='\n',delimiter=",")
+                        reader = csv.reader(csvinput,lineterminator='\n',delimiter=",")
+                        all = []
+                        next(reader,None)
+                        for row in reader:
+                            all.append(row)
+                        writer.writerows(all)
+                        
+                       
+    for filename in os.listdir(save_path):
+         if not filename.startswith("out_"):
+             os.remove(filename)
+
+
 
 #inicializuje browser
 driver=init_driver()
 time.sleep(5)
 
-# zaloguje usera
-lookup(driver,dict['Zbozi.cz']['Login_1']['Login'], dict['Zbozi.cz']['Login_1']['Password'])
-time.sleep(5)
-
-#zavola scraping
-for index in range(0,3):
-    shop_id = dict['Zbozi.cz']['Login_1']['Shop_id'][index]
-    shop_shortcut = dict['Zbozi.cz']['Login_1']['Shop_shortcut'][index]
-    
-    scrape(driver,shop_id,shop_shortcut)
 
 
-# zaloguje usera 2
-lookup(driver,dict['Zbozi.cz']['Login_2']['Login'], dict['Zbozi.cz']['Login_2']['Password'])
-time.sleep(5)
-shop_id = dict['Zbozi.cz']['Login_2']['Shop_id']
-shop_shortcut = dict['Zbozi.cz']['Login_2']['Shop_shortcut']
-    
-scrape(driver,shop_id,shop_shortcut)
+## SCRAPING
+
+if mode == 'by_category':
+    # FOR CYKLUS pres vsechny vybrane scrape_datey v scrape_dates vektoru
+    for i in range(len(stats_dates)):
+        stats_date=stats_dates[i]
 
 
-# zaloguje usera 3
-lookup(driver,dict['Zbozi.cz']['Login_3']['Login'], dict['Zbozi.cz']['Login_3']['Password'])
-time.sleep(5)
-shop_id = dict['Zbozi.cz']['Login_3']['Shop_id']
-shop_shortcut = dict['Zbozi.cz']['Login_3']['Shop_shortcut']
-    
-scrape(driver,shop_id,shop_shortcut)
+        # zaloguje usera 1
+        lookup(driver,dict['Zbozi.cz']['Login_1']['Login'], dict['Zbozi.cz']['Login_1']['Password'])
+        time.sleep(5)
+
+        #for i in range(0,2):
+        #zavola scraping
+        for index in range(0,3):
+            shop_id = dict['Zbozi.cz']['Login_1']['Shop_id'][index]
+            shop_shortcut = dict['Zbozi.cz']['Login_1']['Shop_shortcut'][index]   
+            scrape(driver,shop_id,shop_shortcut,stats_date,stats_date)
+
+
+        # zaloguje usera 2
+        lookup(driver,dict['Zbozi.cz']['Login_2']['Login'], dict['Zbozi.cz']['Login_2']['Password'])
+        time.sleep(5)
+
+        #zavola scraping
+        shop_id = dict['Zbozi.cz']['Login_2']['Shop_id']
+        shop_shortcut = dict['Zbozi.cz']['Login_2']['Shop_shortcut']
+        scrape(driver,shop_id,shop_shortcut,stats_date,stats_date)
+
+
+        # zaloguje usera 3
+        lookup(driver,dict['Zbozi.cz']['Login_3']['Login'], dict['Zbozi.cz']['Login_3']['Password'])
+        time.sleep(5)
+
+
+        #zavola scraping
+        shop_id = dict['Zbozi.cz']['Login_3']['Shop_id']
+        shop_shortcut = dict['Zbozi.cz']['Login_3']['Shop_shortcut']
+        scrape(driver,shop_id,shop_shortcut,stats_date,stats_date)
+
+        #poresi csvcka
+        csv_handeler(mode,stats_date)
+
+
+
+
+if mode == 'summary':
+    date_from=stats_dates[0]
+    date_to=stats_dates[len(stats_dates)-1]
+
+        # zaloguje usera 1
+    lookup(driver,dict['Zbozi.cz']['Login_1']['Login'], dict['Zbozi.cz']['Login_1']['Password'])
+    time.sleep(5)
+
+    #for i in range(0,2):
+    #zavola scraping
+    for index in range(0,3):
+        shop_id = dict['Zbozi.cz']['Login_1']['Shop_id'][index]
+        shop_shortcut = dict['Zbozi.cz']['Login_1']['Shop_shortcut'][index]   
+        scrape(driver,shop_id,shop_shortcut,date_from,date_to)
+
+
+    # zaloguje usera 2
+    lookup(driver,dict['Zbozi.cz']['Login_2']['Login'], dict['Zbozi.cz']['Login_2']['Password'])
+    time.sleep(5)
+
+    #zavola scraping
+    shop_id = dict['Zbozi.cz']['Login_2']['Shop_id']
+    shop_shortcut = dict['Zbozi.cz']['Login_2']['Shop_shortcut']
+    scrape(driver,shop_id,shop_shortcut,date_from,date_to)
+
+
+    # zaloguje usera 3
+    lookup(driver,dict['Zbozi.cz']['Login_3']['Login'], dict['Zbozi.cz']['Login_3']['Password'])
+    time.sleep(5)
+
+
+    #zavola scraping
+    shop_id = dict['Zbozi.cz']['Login_3']['Shop_id']
+    shop_shortcut = dict['Zbozi.cz']['Login_3']['Shop_shortcut']
+    scrape(driver,shop_id,shop_shortcut,date_from,date_to)
+
+    #poresi csvcka
+    csv_handeler(mode,date_from)
+
+
+
+
+## /SCRAPING
 
 
 #zavre browser
@@ -208,30 +357,8 @@ time.sleep(5)
 
 os.chdir(save_path)
 
-for filename in os.listdir(save_path):
-    if filename.endswith(".csv"):
-        with open(save_path+filename,'r') as csvinput:
-            with open(save_path+'out_'+filename, 'w') as csvoutput:
-                writer = csv.writer(csvoutput, lineterminator='\n',delimiter=";")
-                reader = csv.reader(csvinput,delimiter=";")
 
-                all = []
-                row = next(reader)
-                row.append('Date')
-                all.append(row)
-
-                for row in reader:
-                    row.append(datum)
-                    all.append(row)
-
-                writer.writerows(all)
-
-
-for filename in os.listdir(save_path):
-    if not filename.startswith("out_"):
-        os.remove(filename)
-
-
+#test print
 cr = csv.reader(open(save_path+"out_zbozi_stats_bb.csv","rb"))
 for row in cr:    
     print row
